2eaa48da804c0b65ba0643f904902e34b370bead
David Boehm <boehm.david@gmx.at>
2022-08-31 18:38:21 +0200

    Fixed: Crash when no player name entered

--- main.py
+++ main.py
@@ -80,6 +80,8 @@ class Darts():
 def main() -> None:
     ui = CLI()
     players = ui.read_players()
+    if not len(players):
+        sys.exit(f"The game was canceled, no players found")
     game_opt = ui.read_game_options(players)
     game = Darts(ui, players, game_opt)
     game.play()
